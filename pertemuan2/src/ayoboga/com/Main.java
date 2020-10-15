35a06aa099f696c2fd44407b1d0ffdfb2d3cf6c1
randynetworks <randynetworks@gmail.com>
2020-10-15 09:28:34 +0700

    Pertemuan 5 switch case melalui file pertemuan 2

--- pertemuan2/src/ayoboga/com/Main.java
+++ pertemuan2/src/ayoboga/com/Main.java
@@ -5,40 +5,51 @@ public class Main {
     public static void main(String[] args) {
 //        CASE
         int st = 1;
-        System.out.println("1. Nasi Goreng");
-        System.out.println("2. Nasi Pandang");
-        System.out.println("3. Nasi Rames");
+        String serve = null;
+        System.out.println("1. Nasi Goreng " +
+                "\n2. Nasi Pandang" +
+                "\n3. Nasi Rames");
         System.out.print("Pilihan Anda: ");
 
         switch (st) {
             case 1 :
-                System.out.println("Nasi Goreng siap dihidangkan");
+                serve = "Nasi Goreng siap dihidangkan";
+                break;
             case 2 :
-                System.out.println("Nasi Pandang siap dihidangkan");
+                serve = "Nasi Pandang siap dihidangkan";
+                break;
             case 3 :
-                System.out.println("Nasi Rames siap dihidangkan");
+                serve = "Nasi Rames siap dihidangkan";
+                break;
+            default :
+                serve = "Engga ada yang di serve, kamu belum milih";
+                break;
         }
+        
+        System.out.println(serve);
+        
+
 
 
 //      IF statement 1
-        int x;
-        x = -2;
-        if (x < 0) {
-            System.out.println ("ABCD");
-            System.out.println ("EFGH");
-            System.out.println ("XYZ");
-            }
+//        int x;
+//        x = -2;
+//        if (x < 0) {
+//            System.out.println ("ABCD");
+//            System.out.println ("EFGH");
+//            System.out.println ("XYZ");
+//            }
 
     //  IF ELSE IF ELSE
-        char firstInitial = 'a';
-        if (firstInitial == 'a') {
-            System.out.println("Nama anda pasti Asep!");
-        }else if (firstInitial == 'b') {
-            System.out.println("Nama anda pasti Brodin!");
-        }else if (firstInitial == 'c') {
-            System.out.println("Nama anda pasti Cecep!");
-        }else {
-            System.out.println("Nama anda tidak terkenal!");
-        }
+//        char firstInitial = 'a';
+//        if (firstInitial == 'a') {
+//            System.out.println("Nama anda pasti Asep!");
+//        }else if (firstInitial == 'b') {
+//            System.out.println("Nama anda pasti Brodin!");
+//        }else if (firstInitial == 'c') {
+//            System.out.println("Nama anda pasti Cecep!");
+//        }else {
+//            System.out.println("Nama anda tidak terkenal!");
+//        }
     }
 }

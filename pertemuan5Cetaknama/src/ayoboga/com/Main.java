67238401b149203f9649e946715125cccc2c50f9
randynetworks <randynetworks@gmail.com>
2020-10-15 09:47:50 +0700

    pertemuan 5 latihan looping

---
+++ pertemuan5Cetaknama/src/ayoboga/com/Main.java
@@ -0,0 +1,24 @@
+package ayoboga.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	for
+
+        for (int i = 1; i <= 100; i++){
+            System.out.println("Randy Ramadhan");
+        }
+
+//        while
+        int i = 1;
+        while (i <= 100){
+            System.out.println("Randy Ramadhan");
+        }
+
+//        do while
+        int x = 1;
+        do {
+            System.out.println("Randy Ramadhan");
+        } while (i <= 100);
+    }
+}

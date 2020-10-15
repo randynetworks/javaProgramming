73c469906926127f93458826d2f8eb70b72d165e
randynetworks <randynetworks@gmail.com>
2020-10-15 09:41:16 +0700

    PErtemuan5 materi

---
+++ src/ayoboga/com/Main.java
@@ -0,0 +1,26 @@
+package ayoboga.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	while
+//        int x = 0;
+//        while ( x < 50 ) {
+//            System.out.println("Yuigahama Yui is the Best Girl!!");
+//            x++;
+//        }
+
+//        Do While
+//        int x = 0;
+//        do {
+//            System.out.println("Yuigahama Yui is the Best Girl!!");
+//        x++;
+//        } while (x < 50);
+
+//        for
+//        for (int x = 0; x <50; x++){
+//            System.out.println("Yuigahama Yui is the Best Girl!!");
+//        }
+
+    }
+}
